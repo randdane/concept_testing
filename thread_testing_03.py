aa52109b07dc77c530de9d5a177cc26723a5ac80
Randall <whiterd434@gmail.com>
2018-02-13 22:28:54 -0600

    First working condition
    Multiprocessing and threading working correctly, but the queue object is blocking and must be fixed.

--- thread_testing_03.py
+++ thread_testing_03.py
@@ -12,28 +12,36 @@
 
 from multiprocessing import Pool as multi_pool
 from multiprocessing.dummy import Pool as thread_pool
+from multiprocessing import SimpleQueue
 import queue
+import requests
+import urllib3
+urllib3.disable_warnings()
 
-q = queue.Queue
+# q = SimpleQueue()
 
 root_instances = ['a', 'b', 'c', 'd', 'e']  # Start a process for each instance.
 url_dict = {'a': ['http://www.microsoft.com', 'http://www.microsoft.org'], 'b': ['http://www.microsoft.net', 'http://www.microsoft.co.uk'], 'c': ['http://www.microsoft.nl', 'http://www.microsoft.co'], 'c': ['http://www.microsoft.dk', 'http://www.outlook.dk'], 'd': ['http://www.skype.dk', 'http://www.live.dk'], 'e': ['http://www.hotmail.dk', 'http://www.hotmail.se', 'http://www.microsoft.se']}
-
-def search_instances(root_name):
+def search_instances(name):
     global url_dict
     t_pool = thread_pool(15)
-    urls = url_dict[root_name]
-    print(t_pool.map(long_running_search, urls))
+    urls = url_dict[name]
+    t_pool.map(long_running_search, urls)
     t_pool.close() 
     t_pool.join()
     
 def long_running_search(url):
     try:
-        r = requests.get(url, timeout=2)
-        print(r.status_code)
-        return r.status_code, r.url
+        r = requests.get(url, timeout=3, verify=False)
+        # q.put(r.staus_code)
+        print(r.url)
     except:
-        pass
+        print('Failed')
+
+if __name__ == '__main__':
     
-with multi_pool(5) as m_pool:
-    print(m_pool.map(search_instances, root_instances))
+    with multi_pool(5) as m_pool:
+        m_pool.map(search_instances, root_instances)
+        
+#     while not q.empty():
+#         print(q.get())
