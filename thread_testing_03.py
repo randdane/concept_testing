c72ade359b471fba25993dfc70037e174fc42560
Randall <whiterd434@gmail.com>
2018-02-14 22:22:00 -0600

    queue working
    Replaced queue.Queue with multiprocessing.Queue which is thread AND process safe. Queue proved to be working, but process.join method appears to be the real problem.

--- thread_testing_03.py
+++ thread_testing_03.py
@@ -10,38 +10,60 @@
     All results will be collected on a queue.
 """
 
-from multiprocessing import Pool as multi_pool
+from multiprocessing import Process, Queue
 from multiprocessing.dummy import Pool as thread_pool
-from multiprocessing import SimpleQueue
-import queue
+from threading import Thread
 import requests
+import time
 import urllib3
+
 urllib3.disable_warnings()
-
-# q = SimpleQueue()
-
+q = Queue()
 root_instances = ['a', 'b', 'c', 'd', 'e']  # Start a process for each instance.
 url_dict = {'a': ['http://www.microsoft.com', 'http://www.microsoft.org'], 'b': ['http://www.microsoft.net', 'http://www.microsoft.co.uk'], 'c': ['http://www.microsoft.nl', 'http://www.microsoft.co'], 'c': ['http://www.microsoft.dk', 'http://www.outlook.dk'], 'd': ['http://www.skype.dk', 'http://www.live.dk'], 'e': ['http://www.hotmail.dk', 'http://www.hotmail.se', 'http://www.microsoft.se']}
-def search_instances(name):
+
+def search_instances(name, q):
+    """worker function"""
     global url_dict
-    t_pool = thread_pool(15)
     urls = url_dict[name]
-    t_pool.map(long_running_search, urls)
-    t_pool.close() 
-    t_pool.join()
-    
-def long_running_search(url):
+    print(f'Worker {name} started...')
+    thread_list = [Thread(target=long_running_search, args=(url, q)) for url in urls]
+    for thread in thread_list:
+        thread.start()
+    while True:
+        alive = sum(thread.is_alive() for thread in thread_list)
+        if not alive:
+            break
+        print(f'{alive} threads running')
+        time.sleep(1)
+    for thread in thread_list:
+        thread.join()
+    print(q.get())  # Proof of queue working as expected.
+    print(f'Worker {name} done.')
+
+
+def long_running_search(url, q):
     try:
-        r = requests.get(url, timeout=3, verify=False)
-        # q.put(r.staus_code)
-        print(r.url)
-    except:
-        print('Failed')
+        r = requests.get(url, timeout=2, verify=False)
+        q.put(r)
+        #print(url)
+    except Exception as e:
+        print(f'{e}')
+
 
 if __name__ == '__main__':
-    
-    with multi_pool(5) as m_pool:
-        m_pool.map(search_instances, root_instances)
+    jobs = []
+    for root_name in root_instances:
+        p = Process(target=search_instances, args=(root_name, q))
+        jobs.append(p)
+        p.start()
+    print(f'Jobs: {jobs}')
+    for job in jobs:
+        print(job)
+        job.join()  # Wait until all processes have exited.
+        print('Finished joining processes.')  # <-- Not reached.
         
-#     while not q.empty():
-#         print(q.get())
+    while not q.empty():
+        print(q.get())
+        
+    print('Fin')
