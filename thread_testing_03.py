721c9e2bf9ae4fa8b5b326a280844d8dcb1d8e33
Randall White <whiterd434@gmail.com>
2018-02-19 21:26:14 -0600

    Queue fixed and script functioning.
    Sending items to the queue was moved out of the threads and into the parent process. Global was also removed since it would be needlessly copied for each process.

--- thread_testing_03.py
+++ thread_testing_03.py
@@ -1,69 +1,61 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+
 """
-    More dangerous example...
-    
-    Fork 4 - 8 processes for different databases
-    that each limit concurrent connections to 20.
-    
-    Each process with then spawn less than 20 threads.
-    
-    All results will be collected on a queue.
+    Fork and Thread
+    ---------------
+
+    Fork multiple processes dynamically to handle separate environments.
+
+    Create threads to handle all URLs within each process.
+
+    Send results back to main process using a queue.
 """
 
+from concurrent.futures import ThreadPoolExecutor
 from multiprocessing import Process, Queue
-from multiprocessing.dummy import Pool as thread_pool
-from threading import Thread
 import requests
-import time
 import urllib3
 
 urllib3.disable_warnings()
 q = Queue()
-root_instances = ['a', 'b', 'c', 'd', 'e']  # Start a process for each instance.
-url_dict = {'a': ['http://www.microsoft.com', 'http://www.microsoft.org'], 'b': ['http://www.microsoft.net', 'http://www.microsoft.co.uk'], 'c': ['http://www.microsoft.nl', 'http://www.microsoft.co'], 'c': ['http://www.microsoft.dk', 'http://www.outlook.dk'], 'd': ['http://www.skype.dk', 'http://www.live.dk'], 'e': ['http://www.hotmail.dk', 'http://www.hotmail.se', 'http://www.microsoft.se']}
 
-def search_instances(name, q):
+root_instances = ['a', 'b', 'c', 'd', 'e']  # Start a process for each instance.
+url_dict = {'a': ['http://www.microsoft.com', 'http://www.microsoft.org'],
+            'b': ['http://www.microsoft.net', 'http://www.microsoft.co.uk'],
+            'c': ['http://www.microsoft.nl', 'http://www.microsoft.co'],
+            'd': ['http://www.skype.dk', 'http://www.live.dk'],
+            'e': ['http://www.hotmail.dk', 'http://www.hotmail.se', 'http://www.microsoft.se']}
+
+
+def search_instances(name, q, url_list):
     """worker function"""
-    global url_dict
-    urls = url_dict[name]
     print(f'Worker {name} started...')
-    thread_list = [Thread(target=long_running_search, args=(url, q)) for url in urls]
-    for thread in thread_list:
-        thread.start()
-    while True:
-        alive = sum(thread.is_alive() for thread in thread_list)
-        if not alive:
-            break
-        print(f'{alive} threads running')
-        time.sleep(1)
-    for thread in thread_list:
-        thread.join()
-    print(q.get())  # Proof of queue working as expected.
+    with ThreadPoolExecutor() as executor:
+        results = executor.map(long_running_search, url_list)
+        q.put([x for x in results])  # Send results to the shared queue.
     print(f'Worker {name} done.')
 
 
-def long_running_search(url, q):
+def long_running_search(url):
     try:
         r = requests.get(url, timeout=2, verify=False)
-        q.put(r)
-        #print(url)
+        return r.status_code
     except Exception as e:
         print(f'{e}')
 
 
 if __name__ == '__main__':
+
     jobs = []
     for root_name in root_instances:
-        p = Process(target=search_instances, args=(root_name, q))
+        p = Process(target=search_instances, args=(root_name, q, url_dict[root_name]))
         jobs.append(p)
         p.start()
-    print(f'Jobs: {jobs}')
+
     for job in jobs:
-        print(job)
-        job.join()  # Wait until all processes have exited.
-        print('Finished joining processes.')  # <-- Not reached.
-        
+        job.join()  # Ensure each process exits normally.
+
     while not q.empty():
-        print(q.get())
-        
+        print(q.get())  # Retrieve each item from the queue.
+
     print('Fin')
